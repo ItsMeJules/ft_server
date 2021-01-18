9fcddc6118ea8d8a96c04fe99a344d49e676088f
Jules PEYRON <jpeyron@e2r4p6.42.fr>
2021-01-18 13:33:00 +0100

    advancing more and more

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM debian:buster
 
 # Tools needed
-RUN apt update && apt upgrade -y && apt install -y wget
+RUN apt update && apt upgrade -yq && apt install -yq wget
 
 # Needed packages
-RUN apt install -y nginx \
-&& apt -y install php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring \
-&& apt install -y mariadb-server
+RUN apt install -yq nginx \
+&& apt install -yq php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring \
+&& apt install -yq mariadb-server \
+&& apt install -yq openssl
--- srcs/config.sh
+++ srcs/config.sh
@@ -6,6 +6,17 @@ echo "CREATE DATABASE wordpress;" | mysql -u root
 echo "GRANT ALL ON wordpress.* TO 'root'@'localhost';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
+# WordPress setup
+cd /tmp/
+wget -c https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/html
+mv /tmp/wp-config.php /var/www/html/wordpress
+
+# SSL setup
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/site.pem -keyout /etc/nginx/ssl/site.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=rchallie/CN=monsupersite"
+
 # phpMyAdmin
 
 
